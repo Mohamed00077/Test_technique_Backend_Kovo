3cb612ef1a589505927ba2aa0ac745c6d6e3815a
Mohamed00077 <mohamed.diabagate@epitech.eu>
2026-08-09 17:07:02 +0000

    chore: redirect root route to Swagger documentation

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/api/documentation');
 });
 
 Route::get('/login', function () {
